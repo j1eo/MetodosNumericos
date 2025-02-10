ba7fc13a18fbf3865715a4d449a47e0ccac0de7b
j1eo <jleojimenezg12@gmail.com>
2025-02-10 15:15:40 -0600

    first commit

---
+++ Scripts/sliderPunto.js
@@ -0,0 +1,28 @@
+const images = document.querySelectorAll('.image');
+const captionContainer = document.querySelector('.caption-container'); // Ahora selecciona por clase
+const captions = [
+    "Imagen 1",
+    "Imagen 2",
+    "Imagen 3",
+    "Imagen 4"
+];
+let imgIndex = 0;
+
+function updateImage() {
+    images.forEach((img, index) => {
+        img.style.display = index === imgIndex ? 'block' : 'none';
+    });
+    captionContainer.innerHTML = `<p>${captions[imgIndex]}</p>`; // Actualiza el texto de la imagen actual
+}
+
+function previousImg() {
+    imgIndex = imgIndex > 0 ? imgIndex - 1 : images.length - 1;
+    updateImage();
+}
+
+function nextImg() {
+    imgIndex = imgIndex < images.length - 1 ? imgIndex + 1 : 0;
+    updateImage();
+}
+
+updateImage();
